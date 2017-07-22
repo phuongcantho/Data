a1f481be7851003527d993cb0ebb23780c3a616e
phuongcantho <30324154+phuongcantho@users.noreply.github.com>
2017-07-22 11:48:11 +0700

    Delete test.js

--- js-file/test.js
+++
@@ -1,9 +0,0 @@
-function setGuttersForLineNumbers(options) {
-    var found = indexOf(options.gutters, "CodeMirror-linenumbers");
-    if (found == -1 && options.lineNumbers) {
-      options.gutters = options.gutters.concat(["CodeMirror-linenumbers"]);
-    } else if (found > -1 && !options.lineNumbers) {
-      options.gutters = options.gutters.slice(0);
-      options.gutters.splice(found, 1);
-    }
-  }
